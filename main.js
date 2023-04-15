b2189036f5be68f82a7c07b0e212e0366b4304d4
roconn <roconn25@gmail.com>
2023-04-15 17:33:19 +0100

    adding lighting + helper tools

--- main.js
+++ main.js
@@ -2,10 +2,9 @@ import './style.css'
 
 import * as THREE from 'three'
 
+// scene setup inlcudes scene, camera and renderer
 const scene = new THREE.Scene()
-
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000)
-
 const renderer = new THREE.WebGLRenderer({
     canvas: document.querySelector('#bg')
 })
@@ -13,23 +12,35 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setPixelRatio( window.devicePixelRatiom )
 renderer.setSize( window.innerWidth, window.innerHeight )
 camera.position.setZ(30)
-
 renderer.render( scene, camera )
 
-
+// shape and materials
 const geometry = new THREE.TorusGeometry( 10, 3, 16, 100 )
-const material = new THREE.MeshBasicMaterial( { color: 0xe6aace, wireframe: true } )
+const material = new THREE.MeshStandardMaterial( { color: 0xe6aace } )
 const torus = new THREE.Mesh( geometry, material )
 
 scene.add(torus)
 
+// lighting
+const pointLight = new THREE.PointLight(0xffffff)
+pointLight.position.set(5,5,5)
+
+const ambientLight = new THREE.AmbientLight(0xffffff)
+
+scene.add(pointLight, ambientLight)
+
+// lighting help tools
+const lightHelper = new THREE.PointLightHelper(pointLight)
+const gridHelper = new THREE.GridHelper(200, 50)
+scene.add(lightHelper, gridHelper)
+
+// animation loop function
 function animate() {
     requestAnimationFrame( animate )
 
     torus.rotation.x += 0.01
     torus.rotation.y += 0.005
     torus.rotation.z += 0.01
-
 
     renderer.render( scene, camera )
 }
